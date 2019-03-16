82906985465de4864803e67baa4826510cfcf002
faskowit <jfaskowi@iu.edu>
2019-03-16 17:30:35 -0400

    added check for prob=0

--- src/tools/get_comm_motif_entropy.m
+++ src/tools/get_comm_motif_entropy.m
@@ -16,15 +16,39 @@ if nargin < 5
     pMat2 = pMat ./ tot ;
     dMat2 = dMat ./ tot ;
     
-    entMat = -1 .* ( aMat2.*log2(aMat2) + ...
-            cMat2.*log2(cMat2) + ...
-            pMat2.*log2(pMat2) + ...
-            dMat2.*log2(dMat2) ) ; 
+    entMat = -1 .* ( aMat2.*log2or0(aMat2) + ...
+            cMat2.*log2or0(cMat2) + ...
+            pMat2.*log2or0(pMat2) + ...
+            dMat2.*log2or0(dMat2) ) ; 
 else
-    entMat = -1 .* ( aMat.*log2(aMat) + ...
-        cMat.*log2(cMat) + ...
-        pMat.*log2(pMat) + ...
-        dMat.*log2(dMat) + ...
-        odMat.*log2(odMat)) ; 
+    % check to make sure the sums add to 1
+    check_probs(cat(3,aMat,cMat,pMat,dMat,odMat))
+    
+    entMat = -1 .* ( aMat.*log2or0(aMat) + ...
+        cMat.*log2or0(cMat) + ...
+        pMat.*log2or0(pMat) + ...
+        dMat.*log2or0(dMat) + ...
+        odMat.*log2or0(odMat)) ; 
 end
 
+function check_probs(stackedMat)
+    % check within a tol (because matlab has decimal issues sometimes)
+    
+    sums = sum(stackedMat,3) ;
+    
+    % get nonzero
+    nz = nonzeros(sums) ;
+    ismem = abs(ones(length(nz),1) - nz) < 10e-6 ;
+
+    if sum(ismem) ~= length(ismem)
+       error('probabilies not good') 
+    end
+end
+
+function out = log2or0(in)  
+    % function to calculate log2, but if 0, return 0
+    out = log2(in) ;
+    out(isinf(out)) = 0 ;
+end
+
+end % main function
